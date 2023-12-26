2ff99f0a9812db125776bade35da718b04d17779
omkarnimase02 <142877485+omkarnimase02@users.noreply.github.com>
2023-12-26 11:50:24 +0000

    summary

---
+++ .devcontainer/01_basic/dataTypes-Summary.js
@@ -0,0 +1,34 @@
+
+console.log("Omkar Nimase");
+
+//. Symbvol -> for uniquckly identify
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(typeof id);
+// console.log(id === anotherid);
+
+// BigInt
+
+const bigint = 97276691998n
+// console.log(typeof bigint);
+
+// Array
+const arr =["omkar", "pravin","love"];
+console.log(typeof arr);
+console.log(arr);
+
+const myObj = {
+    name : "Omkar",
+    age : 21,
+}
+
+console.log(typeof myObj);
+console.log(myObj);
+
+// Function
+  const myFunction= function(){
+    console.log("Hello  omkar");
+  }
+  console.log(typeof myFunction);
+  console.log(myFunction);
