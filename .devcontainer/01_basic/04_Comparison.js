79a31fbc47f68881894bedc3fc0ddf314c44e9d6
omkarnimase02 <142877485+omkarnimase02@users.noreply.github.com>
2023-12-26 11:07:27 +0000

    Comparison Done

---
+++ .devcontainer/01_basic/04_Comparison.js
@@ -0,0 +1,27 @@
+
+console.log("Omkar Nimase");
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+
+
+// Comparision of NULL
+
+
+// console.log(null >0);
+// console.log(null <0);
+// console.log(null >= 0);
+// console.log(null ==0);
+
+console.log(undefined ==0);
+console.log(undefined >0);
+console.log(undefined < 0);
+
+
+// strict Check -> ===  -> check the datatype also
+
+// console.log("2" == 2);
+// console.log("2" === 2);
