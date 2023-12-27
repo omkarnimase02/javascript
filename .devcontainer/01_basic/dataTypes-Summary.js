34b3fef7e1635e87e192440e6da272f70a48124c
omkarnimase02 <142877485+omkarnimase02@users.noreply.github.com>
2023-12-27 04:48:25 +0000

    string methods

--- .devcontainer/01_basic/dataTypes-Summary.js
+++ .devcontainer/01_basic/dataTypes-Summary.js
@@ -5,7 +5,7 @@ console.log("Omkar Nimase");
 
 const id = Symbol('123')
 const anotherid = Symbol('123')
-console.log(typeof id);
+// console.log(typeof id);
 // console.log(id === anotherid);
 
 // BigInt
@@ -15,20 +15,69 @@ const bigint = 97276691998n
 
 // Array
 const arr =["omkar", "pravin","love"];
-console.log(typeof arr);
-console.log(arr);
+// console.log(typeof arr);
+// console.log(arr);
 
 const myObj = {
     name : "Omkar",
     age : 21,
 }
 
-console.log(typeof myObj);
-console.log(myObj);
+// console.log(typeof myObj);
+// console.log(myObj);
 
 // Function
   const myFunction= function(){
     console.log("Hello  omkar");
   }
-  console.log(typeof myFunction);
-  console.log(myFunction);
+  // console.log(typeof myFunction);
+  // console.log(myFunction);
+
+
+  // ****************    Memory ********************
+
+  // STACK --> Static    All primitive data types are store in the stack
+             //   if new is created using the old one then they pass as copy 
+            // original  not affect the new one
+  
+            
+   // HEAP -> Dynamic used in all non - primitive data types
+            //  pointing to the same references
+            // onr change affect the other
+
+// example
+
+// stack
+let name ="Omkar"
+
+let antherName = name
+console.log(name)
+console.log(antherName);
+
+antherName = "NImase"
+console.log(antherName);
+console.log(name);
+
+
+// Heap -> references   -> one change other also change
+
+let userOne ={
+  name:"Omkar",
+  age: 21,
+}
+
+let userTwo = userOne
+console.log(userOne);
+console.log(userTwo);
+
+userTwo.name="Prerna"
+
+console.log(userOne.name);
+console.log(userTwo.name);
+
+
+
+
+
+
+
