8cf9b03da992cae7481fd17a18cd507569be0ee8
omkarnimase02 <142877485+omkarnimase02@users.noreply.github.com>
2024-01-22 04:25:41 +0000

    json talk

--- .devcontainer/02_Basic/04_Objects.js
+++ .devcontainer/02_Basic/04_Objects.js
@@ -54,17 +54,62 @@ const obj2 ={3 :"c",4:"d"}
 
 // 3 ->  Mergge usiong the spread ...
 const obj3 ={...obj1, ...obj2};
-console.log(obj3);
+// console.log(obj3);
 
 
 // Find out the all keys in the objects
-console.log(Object.keys(tinder));  // print in the arr format
+// console.log(Object.keys(tinder));  // print in the arr format
 
 // also the values
-console.log(Object.values(tinder));   // all values in arr format
+// console.log(Object.values(tinder));   // all values in arr format
 
 // Entries
-console.log(Object.entries(tinder)); // arr inside arr -> key & its value in sub arr
+// console.log(Object.entries(tinder)); // arr inside arr -> key & its value in sub arr
 
 // given property is present or not
-console.log(tinder.hasOwnProperty('name')); // return in the boolean
+// console.log(tinder.hasOwnProperty('name')); // return in the boolean
+
+
+// De- Structor of objects
+
+const course={
+    name :"Chay our Code",
+    price:"999",
+    courseInstructor :" Omkar"
+}
+
+// suppose print name 3 times then to write the log statement
+// console.log(course.name);
+// console.log(course.name);
+// console.log(course.name);
+
+// De- structure
+
+// const {courseInstructor} = course;
+// console.log(courseInstructor);
+
+
+// also set the name
+const {courseInstructor:instructor} = course;
+console.log(instructor);  /// call its using the its name
+
+// API `s
+
+// JSON talk 
+
+// API in object forms
+
+// all  key & value store in the string format
+// {
+//     "name" :"Omkar",
+//     "no" :"29"
+// }
+
+// Also in arr format
+
+// [
+
+//     {}
+//     {}
+//     {}
+// ]
